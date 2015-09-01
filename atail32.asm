da448c060a8b1bf8c52ef6c701a1e2dd59769d77
Bijal Thanawala <bijalthanawala@users.noreply.github.com>
2015-09-01 11:38:21 +1000

    Determines and stores length of the filename. Huge improvement in register usage/convention

--- atail32.asm
+++ atail32.asm
@@ -5,6 +5,10 @@ segment .text
 ;
 ; Procedure:	main
 ;
+; Parameters:	The standard 'C' main() parameters
+;		Recall the prototype, 
+;		int main(int argc, char *argv[], char *env[])
+;
 main:
 	; Set variables: argc, argv
 	mov ebp, esp	
@@ -15,40 +19,63 @@ main:
 	mov ebx, [ebp]		; Fetch argv
 	mov [argv], ebx
 	
-	; Isolate filename from the arguments
-	call get_fnameptr
+	; Get information about the filename
+	call get_fnameinfo
 	mov [fnameptr], eax
+	mov [fnamelen], cx
 	
 	; Print the filename
 	call print_fname
+
+	; Gracefuly exit main()
+	mov eax, 0
         ret	
 
 ;
-; Procedure:	get_fnameptr
-;		Isolates filename from the commandline 
-;		arguments
-; Returns:	Ptr to filename in EAX
-get_fnameptr:
+; Procedure:	get_fnameinfo
+;		Isolates filename pointer from the commandline arguments, 
+;		returns that and the length of the filename
+; Expects:	Variable argv to hold valid argv pointer
+;
+; Returns:	Pointer to the filename in EAX
+;		Length of the filename in CX (Note: CX, not ECX)
+;
+get_fnameinfo:
 	mov eax, [argv]
-	mov eax, [eax+4]
+	mov eax, [ebx+4]
+	call get_fname_len
+	ret
+
+;
+; Procedure:	get_fname_len
+;
+; Parameter:    eax: Pointer to the filename
+;
+; Returns:	cx: Length of the filename (excluding NULL)
+;
+get_fname_len:
+	push edi
+	xor cx, cx
+	mov edi, eax
+loop_print_fname:
+	cmp byte [edi], 0
+	jz get_fname_len_end
+	inc cx
+	inc edi
+	jmp loop_print_fname
+get_fname_len_end:
+	pop edi
 	ret
 
 ;
 ; Procedure:	print_fname
 ;
 print_fname:
-	mov edi, [fnameptr]
-	mov ecx, edi 
-loop_print_fname:
-	cmp byte [ecx], 0
-	jz print_fname_end
 	mov eax, 4
 	mov ebx, 1
-	mov edx, 1
+	mov ecx, [fnameptr]
+	mov edx, [fnamelen] 
 	int 80h
-	inc ecx
-	jmp loop_print_fname
-print_fname_end:
 	ret
 
 
@@ -57,3 +84,4 @@ segment .data
 	argc dd 0
 	argv dd 0
 	fnameptr dd 0
+	fnamelen dw 0
